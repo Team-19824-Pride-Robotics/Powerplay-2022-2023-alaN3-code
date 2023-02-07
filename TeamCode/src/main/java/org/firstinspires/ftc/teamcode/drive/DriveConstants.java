5474d4c1b21b9a906a7d9969016ffe0e505011bf
Yajie Li <104532860+YoshiVox1@users.noreply.github.com>
2023-02-07 17:02:08 -0600

    update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -53,9 +53,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.508248 / rpmToVelocity(MAX_RPM);
-    public static double kA = 0.00001;
-    public static double kStatic = 0.05244;
+    public static double kV = 2.4697 / rpmToVelocity(MAX_RPM);
+    public static double kA = .001;
+    public static double kStatic = .03;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
@@ -33,10 +33,10 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
     public static double LATERAL_DISTANCE = 6.5; // in; distance between the left and right wheels
-    public static double FORWARD_OFFSET = -6; // in; offset of the lateral wheel
+    public static double FORWARD_OFFSET = 2.5; // in; offset of the lateral wheel
 
-    public static double X_MULTIPLIER = 0.98742228; // Multiplier in the X direction
-    public static double Y_MULTIPLIER = 1.00898593; // Multiplier in the Y direction
+    public static double X_MULTIPLIER = 1.0165841; // Multiplier in the X direction
+    public static double Y_MULTIPLIER = 1.04025031; // Multiplier in the Y direction
 
     private Encoder leftEncoder, rightEncoder, frontEncoder;
 
@@ -47,12 +47,12 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
                 new Pose2d(FORWARD_OFFSET, 1.5, Math.toRadians(90)) // front
         ));
 
-        leftEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "LF"));
+        leftEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "RB"));
         rightEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "RF"));
-        frontEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "RB"));
+        frontEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "LF"));
 
         // TODO: reverse any encoders using Encoder.setDirection(Encoder.Direction.REVERSE)
-        //frontEncoder.setDirection(Encoder.Direction.REVERSE);
+        frontEncoder.setDirection(Encoder.Direction.REVERSE);
     }
 
     public static double encoderTicksToInches(double ticks) {
